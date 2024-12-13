56d8f819453fae954141a9ee5b49a6959e9268f3
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-12-13 11:31:28 -0500

    put out materials + new baby_geometry_function

--- analysis/neutron/openmc_model.py
+++ analysis/neutron/openmc_model.py
@@ -1,131 +1,11 @@
 import openmc
 import vault
-import numpy as np
-import matplotlib.pyplot as plt
 import helpers
 
 
-def baby_model():
-    ############################################################################
-    # Define Materials
-    # Source: PNNL Materials Compendium April 2021
-    # PNNL-15870, Rev. 2
-    inconel625 = openmc.Material(name="Inconel 625")
-    inconel625.add_element("C", 0.000990, "wo")
-    inconel625.add_element("Al", 0.003960, "wo")
-    inconel625.add_element("Si", 0.004950, "wo")
-    inconel625.add_element("P", 0.000148, "wo")
-    inconel625.add_element("S", 0.000148, "wo")
-    inconel625.add_element("Ti", 0.003960, "wo")
-    inconel625.add_element("Cr", 0.215000, "wo")
-    inconel625.add_element("Mn", 0.004950, "wo")
-    inconel625.add_element("Fe", 0.049495, "wo")
-    inconel625.add_element("Co", 0.009899, "wo")
-    inconel625.add_element("Ni", 0.580000, "wo")
-    inconel625.add_element("Nb", 0.036500, "wo")
-    inconel625.add_element("Mo", 0.090000, "wo")
-    inconel625.set_density("g/cm3", 8.44)
-
-    # lif-licl - natural - pure
-    licl_frac = 0.695
-    cllif_nat = openmc.Material(name="ClLiF natural")
-    cllif_nat.add_element("F", 0.5 * (1 - licl_frac), "ao")
-    cllif_nat.add_element("Li", 0.5 * (1 - licl_frac) + 0.5 * licl_frac, "ao")
-    cllif_nat.add_element("Cl", 0.5 * licl_frac, "ao")
-    cllif_nat.set_density(
-        "g/cm3", helpers.get_exp_cllif_density(650)
-    )  # 69.5 at. % LiCL at 650 C
-
-    # Stainless Steel 304 from PNNL Materials Compendium (PNNL-15870 Rev2)
-    SS304 = openmc.Material(name="Stainless Steel 304")
-    # SS304.temperature = 700 + 273
-    SS304.add_element("C", 0.000800, "wo")
-    SS304.add_element("Mn", 0.020000, "wo")
-    SS304.add_element("P", 0.000450, "wo")
-    SS304.add_element("S", 0.000300, "wo")
-    SS304.add_element("Si", 0.010000, "wo")
-    SS304.add_element("Cr", 0.190000, "wo")
-    SS304.add_element("Ni", 0.095000, "wo")
-    SS304.add_element("Fe", 0.683450, "wo")
-    SS304.set_density("g/cm3", 8.00)
-
-    heater_mat = openmc.Material(name="heater")
-    heater_mat.add_element("C", 0.000990, "wo")
-    heater_mat.add_element("Al", 0.003960, "wo")
-    heater_mat.add_element("Si", 0.004950, "wo")
-    heater_mat.add_element("P", 0.000148, "wo")
-    heater_mat.add_element("S", 0.000148, "wo")
-    heater_mat.add_element("Ti", 0.003960, "wo")
-    heater_mat.add_element("Cr", 0.215000, "wo")
-    heater_mat.add_element("Mn", 0.004950, "wo")
-    heater_mat.add_element("Fe", 0.049495, "wo")
-    heater_mat.add_element("Co", 0.009899, "wo")
-    heater_mat.add_element("Ni", 0.580000, "wo")
-    heater_mat.add_element("Nb", 0.036500, "wo")
-    heater_mat.add_element("Mo", 0.090000, "wo")
-    heater_mat.set_density("g/cm3", 2.44)
-
-    # Using Microtherm with 1 a% Al2O3, 27 a% ZrO2, and 72 a% SiO2
-    # https://www.foundryservice.com/product/microporous-silica-insulating-boards-mintherm-microtherm-1925of-grades/
-    firebrick = openmc.Material(name="Firebrick")
-    # Estimate average temperature of Firebrick to be around 300 C
-    # Firebrick.temperature = 273 + 300
-    firebrick.add_element("Al", 0.004, "ao")
-    firebrick.add_element("O", 0.666, "ao")
-    firebrick.add_element("Si", 0.240, "ao")
-    firebrick.add_element("Zr", 0.090, "ao")
-    firebrick.set_density("g/cm3", 0.30)
-
-    # alumina insulation
-    # data from https://precision-ceramics.com/materials/alumina/
-    alumina = openmc.Material(name="Alumina insulation")
-    alumina.add_element("O", 0.6, "ao")
-    alumina.add_element("Al", 0.4, "ao")
-    alumina.set_density("g/cm3", 3.98)
-
-    # air
-    air = openmc.Material(name="Air")
-    air.add_element("C", 0.00012399, "wo")
-    air.add_element("N", 0.75527, "wo")
-    air.add_element("O", 0.23178, "wo")
-    air.add_element("Ar", 0.012827, "wo")
-    air.set_density("g/cm3", 0.0012)
-
-    # epoxy
-    epoxy = openmc.Material(name="Epoxy")
-    epoxy.add_element("C", 0.70, "wo")
-    epoxy.add_element("H", 0.08, "wo")
-    epoxy.add_element("O", 0.15, "wo")
-    epoxy.add_element("N", 0.07, "wo")
-    epoxy.set_density("g/cm3", 1.2)
-
-    # helium @5psig
-    pressure = 34473.8  # Pa ~ 5 psig
-    temperature = 300  # K
-    R_he = 2077  # J/(kg*K)
-    density = pressure / (R_he * temperature) / 1000  # in g/cm^3
-    he = openmc.Material(name="Helium")
-    he.add_element("He", 1.0, "ao")
-    he.set_density("g/cm3", density)
-
-    materials = [
-        inconel625,
-        cllif_nat,
-        SS304,
-        heater_mat,
-        firebrick,
-        alumina,
-        air,
-        epoxy,
-        he,
-    ]
-
+def baby_geometry(x_c: float, y_c: float, z_c: float):
     ############################################################################
     # Define Geometry
-    # BABY coordinates
-    x_c = 587
-    y_c = 60
-    z_c = 100
 
     epoxy_thickness = 2.54  # 1 inch
     alumina_compressed_thickness = 2.54  # 1 inch
@@ -357,6 +237,28 @@ def baby_model():
         sphere_cell,
     ]
 
+    return sphere, cllif_cell, cells
+
+
+def baby_model():
+
+    materials = [
+        inconel625,
+        cllif_nat,
+        SS304,
+        heater_mat,
+        firebrick,
+        alumina,
+        air,
+        epoxy,
+        he,
+    ]
+
+    # BABY coordinates
+    x_c = 587
+    y_c = 60
+    z_c = 100
+    sphere, cllif_cell, cells = baby_geometry(x_c, y_c, z_c)
     ############################################################################
     # Define Settings
     point = openmc.stats.Point((x_c, y_c, z_c - 5.635))
@@ -393,6 +295,109 @@ def baby_model():
     )
 
     return model
+
+
+############################################################################
+# Define Materials
+# Source: PNNL Materials Compendium April 2021
+# PNNL-15870, Rev. 2
+inconel625 = openmc.Material(name="Inconel 625")
+inconel625.add_element("C", 0.000990, "wo")
+inconel625.add_element("Al", 0.003960, "wo")
+inconel625.add_element("Si", 0.004950, "wo")
+inconel625.add_element("P", 0.000148, "wo")
+inconel625.add_element("S", 0.000148, "wo")
+inconel625.add_element("Ti", 0.003960, "wo")
+inconel625.add_element("Cr", 0.215000, "wo")
+inconel625.add_element("Mn", 0.004950, "wo")
+inconel625.add_element("Fe", 0.049495, "wo")
+inconel625.add_element("Co", 0.009899, "wo")
+inconel625.add_element("Ni", 0.580000, "wo")
+inconel625.add_element("Nb", 0.036500, "wo")
+inconel625.add_element("Mo", 0.090000, "wo")
+inconel625.set_density("g/cm3", 8.44)
+
+# lif-licl - natural - pure
+licl_frac = 0.695
+cllif_nat = openmc.Material(name="ClLiF natural")
+cllif_nat.add_element("F", 0.5 * (1 - licl_frac), "ao")
+cllif_nat.add_element("Li", 0.5 * (1 - licl_frac) + 0.5 * licl_frac, "ao")
+cllif_nat.add_element("Cl", 0.5 * licl_frac, "ao")
+cllif_nat.set_density(
+    "g/cm3", helpers.get_exp_cllif_density(650)
+)  # 69.5 at. % LiCL at 650 C
+
+# Stainless Steel 304 from PNNL Materials Compendium (PNNL-15870 Rev2)
+SS304 = openmc.Material(name="Stainless Steel 304")
+# SS304.temperature = 700 + 273
+SS304.add_element("C", 0.000800, "wo")
+SS304.add_element("Mn", 0.020000, "wo")
+SS304.add_element("P", 0.000450, "wo")
+SS304.add_element("S", 0.000300, "wo")
+SS304.add_element("Si", 0.010000, "wo")
+SS304.add_element("Cr", 0.190000, "wo")
+SS304.add_element("Ni", 0.095000, "wo")
+SS304.add_element("Fe", 0.683450, "wo")
+SS304.set_density("g/cm3", 8.00)
+
+heater_mat = openmc.Material(name="heater")
+heater_mat.add_element("C", 0.000990, "wo")
+heater_mat.add_element("Al", 0.003960, "wo")
+heater_mat.add_element("Si", 0.004950, "wo")
+heater_mat.add_element("P", 0.000148, "wo")
+heater_mat.add_element("S", 0.000148, "wo")
+heater_mat.add_element("Ti", 0.003960, "wo")
+heater_mat.add_element("Cr", 0.215000, "wo")
+heater_mat.add_element("Mn", 0.004950, "wo")
+heater_mat.add_element("Fe", 0.049495, "wo")
+heater_mat.add_element("Co", 0.009899, "wo")
+heater_mat.add_element("Ni", 0.580000, "wo")
+heater_mat.add_element("Nb", 0.036500, "wo")
+heater_mat.add_element("Mo", 0.090000, "wo")
+heater_mat.set_density("g/cm3", 2.44)
+
+# Using Microtherm with 1 a% Al2O3, 27 a% ZrO2, and 72 a% SiO2
+# https://www.foundryservice.com/product/microporous-silica-insulating-boards-mintherm-microtherm-1925of-grades/
+firebrick = openmc.Material(name="Firebrick")
+# Estimate average temperature of Firebrick to be around 300 C
+# Firebrick.temperature = 273 + 300
+firebrick.add_element("Al", 0.004, "ao")
+firebrick.add_element("O", 0.666, "ao")
+firebrick.add_element("Si", 0.240, "ao")
+firebrick.add_element("Zr", 0.090, "ao")
+firebrick.set_density("g/cm3", 0.30)
+
+# alumina insulation
+# data from https://precision-ceramics.com/materials/alumina/
+alumina = openmc.Material(name="Alumina insulation")
+alumina.add_element("O", 0.6, "ao")
+alumina.add_element("Al", 0.4, "ao")
+alumina.set_density("g/cm3", 3.98)
+
+# air
+air = openmc.Material(name="Air")
+air.add_element("C", 0.00012399, "wo")
+air.add_element("N", 0.75527, "wo")
+air.add_element("O", 0.23178, "wo")
+air.add_element("Ar", 0.012827, "wo")
+air.set_density("g/cm3", 0.0012)
+
+# epoxy
+epoxy = openmc.Material(name="Epoxy")
+epoxy.add_element("C", 0.70, "wo")
+epoxy.add_element("H", 0.08, "wo")
+epoxy.add_element("O", 0.15, "wo")
+epoxy.add_element("N", 0.07, "wo")
+epoxy.set_density("g/cm3", 1.2)
+
+# helium @5psig
+pressure = 34473.8  # Pa ~ 5 psig
+temperature = 300  # K
+R_he = 2077  # J/(kg*K)
+density = pressure / (R_he * temperature) / 1000  # in g/cm^3
+he = openmc.Material(name="Helium")
+he.add_element("He", 1.0, "ao")
+he.set_density("g/cm3", density)
 
 
 if __name__ == "__main__":
