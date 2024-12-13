0b846da02fb8a67715e2dd02c3efc06265c4b73d
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-12-13 14:41:34 -0500

    using measured TBR instead of openmc TBR

--- analysis/tritium/tritium_model.py
+++ analysis/tritium/tritium_model.py
@@ -1,4 +1,4 @@
-from libra_toolbox.tritium.model import ureg, Model
+from libra_toolbox.tritium.model import ureg, Model, quantity_to_activity
 import numpy as np
 import json
 from libra_toolbox.tritium.lsc_measurements import (
@@ -441,39 +441,13 @@ replacement_times_top = sorted(replacement_times_top)
 replacement_times_walls = sorted(replacement_times_walls)
 
 
-# Model
+# tritium model
 
 baby_diameter = 14 * ureg.cm  # TODO confirm with CAD
 baby_radius = 0.5 * baby_diameter
 baby_volume = 1 * ureg.L
 baby_cross_section = np.pi * baby_radius**2
 baby_height = baby_volume / baby_cross_section
-
-# from OpenMC
-
-# read from file
-
-from pathlib import Path
-
-filename = "../neutron/statepoint.100.h5"
-filename = Path(filename)
-
-if not filename.exists():
-    raise FileNotFoundError(f"{filename} does not exist, run OpenMC first")
-
-import openmc
-
-sp = openmc.StatePoint(filename)
-tally_df = sp.get_tally(name="TBR").get_pandas_dataframe()
-calculated_TBR = tally_df["mean"].iloc[0] * ureg.particle * ureg.neutron**-1
-calculated_TBR_std_dev = (
-    tally_df["std. dev."].iloc[0] * ureg.particle * ureg.neutron**-1
-)
-# calculated_TBR = 1.9e-3 * ureg.particle * ureg.neutron**-1
-
-optimised_ratio = 1.7e-2
-k_top = 8.9e-8 * ureg.m * ureg.s**-1
-k_wall = optimised_ratio * k_top
 
 # read irradiation times from general.json
 with open("../../data/general.json", "r") as f:
@@ -491,7 +465,7 @@ for generator in general_data["timestamps"]["generators"]:
     irr_stop_time = irr_stop_time.total_seconds() * ureg.second
     irradiations.append([irr_start_time, irr_stop_time])
 
-
+# Neutron rate
 # calculated from Kevin's activation foil analysis from run 100 mL #7
 # TODO replace for values for this run
 P383_neutron_rate = 4.95e8 * ureg.neutron * ureg.s**-1
@@ -502,10 +476,48 @@ neutron_rate = (
     A325_neutron_rate
 ) / 2  # the neutron rate is divided by two to acount for the double counting (two detectors)
 
+# TBR from OpenMC
+
+from pathlib import Path
+
+filename = "../neutron/statepoint.100.h5"
+filename = Path(filename)
+
+if not filename.exists():
+    raise FileNotFoundError(f"{filename} does not exist, run OpenMC first")
+
+import openmc
+
+sp = openmc.StatePoint(filename)
+tally_df = sp.get_tally(name="TBR").get_pandas_dataframe()
+calculated_TBR = tally_df["mean"].iloc[0] * ureg.particle * ureg.neutron**-1
+calculated_TBR_std_dev = (
+    tally_df["std. dev."].iloc[0] * ureg.particle * ureg.neutron**-1
+)
+
+# TBR from measurements
+
+total_irradiation_time = sum([irr[1] - irr[0] for irr in irradiations])
+
+T_consumed = neutron_rate * total_irradiation_time
+T_produced = (
+    IV_stream.get_cumulative_activity("total")[-1]
+    + OV_stream.get_cumulative_activity("total")[-1]
+)
+
+measured_TBR = (T_produced / quantity_to_activity(T_consumed)).to(
+    ureg.particle * ureg.neutron**-1
+)
+
+optimised_ratio = 1.7e-2
+k_top = 8.9e-8 * ureg.m * ureg.s**-1
+k_wall = optimised_ratio * k_top
+
+
 baby_model = Model(
     radius=baby_radius,
     height=baby_height,
-    TBR=calculated_TBR,
+    TBR=measured_TBR,
     neutron_rate=neutron_rate,
     irradiations=irradiations,
     k_top=k_top,
